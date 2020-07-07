fdb5258e12d783698119e88fdee8cd4d0d4ed6de
shahdhanesh <shhah.dhanesh1@gmail.com>
2020-07-06 22:25:09 -0500

    first commit

---
+++ hungarian.py
@@ -0,0 +1,20 @@
+import numpy as np
+def cluster_acc(y_true, y_pred):
+    """
+        Calculate clustering accuracy. Require scikit-learn installed
+        # Arguments
+            y: true labels, numpy.array with shape `(n_samples,)`
+            y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+        # Return
+            accuracy, in [0,1]
+    """
+    y_true = np.array(y_true, dtype=np.int64)
+    y_pred = np.array(y_pred, dtype=np.int64)
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+    from sklearn.utils.linear_assignment_ import linear_assignment
+    ind = linear_assignment(w.max() - w)
+    return (float(sum([w[i, j] for i, j in ind]))/y_pred.size)*100
---
+++ k-means.py
@@ -0,0 +1,43 @@
+from pylab import plot,show
+from numpy import vstack,array, int32
+from numpy.random import rand
+# from scipy.cluster.vq import kmeans,vq
+from sklearn.cluster import KMeans
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import hungarian
+import data_handler
+
+filename = "HandWrittenLetters.txt"
+number_per_class = 39
+test_instances = "1:3"
+nu_clusters = 26
+
+trainX, trainY, testX, testY = data_handler.splitData2TestTrain(filename, number_per_class, test_instances)
+
+trainX.extend(testX)
+
+trainX = array(trainX, int32)
+testX = array(testX, int32)
+# colors = ["g.","r.","c.","y.", "w.", "b.", "a.", "e.", "f.", "h."]
+kmeans = KMeans (n_clusters=nu_clusters)
+kmeans.fit(trainX)
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
+
+# for i in range(len(trainX)):
+    # print("coordinate:",trainX[i], "label:", labels[i])
+    # plt.plot(trainX[i][0], trainX[i][1],"bo", markersize = 10)
+
+# plt.scatter(centroids[:, 0],centroids[:, 1], marker = "x", s=150, linewidths = 5, zorder = 10)
+# plt.show()
+
+predictions = kmeans.predict(testX)
+predictions = [str(item) for item in predictions]
+
+cm = confusion_matrix(testY, predictions)
+
+acc = hungarian.cluster_acc(testY, predictions)
+print(cm)
+print(acc)
